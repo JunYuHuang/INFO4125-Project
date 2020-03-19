824646c253a292dd0e54a8a133c81b702bd1f516
Jun Huang <junyu_huang@hotmail.com>
2020-03-19 01:12:41 -0700

    complete products page and basic functions for displaying and filtering each product

--- app.js
+++ app.js
@@ -48,3 +48,111 @@ function toggleNavButton() {
   );
   navMenu.classList.toggle("nav-menu--active");
 }
+
+// products page
+
+// front-end mockup products (use until database connection is good)
+const pensArray = [
+  {
+    id: "1",
+    name: "Ballpoint Pen",
+    price: 4.99,
+    description: "The classic choice for students since 1950.",
+    imageURL: "./images/products/ballpoint-1_no_logo.jpg"
+    // inkColours: ["black", "blue", "red"],
+  },
+  {
+    id: "2",
+    name: "Marker Pen",
+    price: 4.99,
+    description:
+      "Great for writing big and bold letters. Also great for smelling.",
+    imageURL: "./images/products/marker-1_no_logo.jpg"
+  },
+  {
+    id: "3",
+    name: "Gel Pen",
+    price: 1.99,
+    description: "An applicator for highly viscous and coloured fluids.",
+    imageURL: "./images/products/gel-1.jpg"
+  },
+  {
+    id: "4",
+    name: "Retractable Pen",
+    price: 1.99,
+    description:
+      "The best tool for both writing and annoying your colleagues at the same time",
+    imageURL: "./images/products/retractable-1.jpg"
+  },
+  {
+    id: "5",
+    name: "Stylus Pen",
+    price: 14.99,
+    description:
+      "Excellent pen with a comfortable silicon rubber grip that allows for precise ink strokes.",
+    imageURL: "./images/products/stylus-1.jpg"
+  },
+  {
+    id: "6",
+    name: "Coming Soon TM",
+    price: 9999.99,
+    description: "Top secret pen coming next century.",
+    imageURL: "./images/products/unknown-product.svg"
+  }
+];
+
+const productSearchForm = document.querySelector(".search-form");
+const productSearchInput = document.querySelector(".input--search");
+const productCardGrid = document.querySelector(".card-grid-container");
+
+const errorMessage = "not applicable";
+
+productSearchForm.addEventListener("submit", e => {
+  e.preventDefault(); // stop form from submitting or refreshing
+});
+
+productSearchInput.addEventListener("keyup", e => {
+  filterProducts(e.target.value);
+});
+
+function filterProducts(searchTerm) {
+  searchTerm = searchTerm.toLowerCase();
+  let allProducts = document.querySelectorAll(".card");
+  allProducts.forEach(product => {
+    const productName = product.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.firstChild.textContent.toLowerCase();
+
+    // hide products that don't match the search term
+    if (productName.indexOf(searchTerm) !== -1) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+}
+
+function displayProductItems() {
+  pensArray.map(pen => {
+    // // onClick={() => setCurrentCountry(country)}
+    let card = `
+      <a
+        href="#"
+        class="card"
+      >
+        <div class="card__img-container">
+          <img src="${pen.imageURL}" alt="An image of a ${pen.name}"} />
+        </div>
+        <section class="card__text ellipsis">
+          <h3 class="card__text__title ellipsis">
+            ${pen.name ? pen.name : errorMessage}
+          </h3>
+          <p class="card__text__price">
+            &#36;${pen.price ? pen.price : errorMessage}
+          </p>
+        </section>
+      </a>
+  `;
+    productCardGrid.insertAdjacentHTML("beforeend", card);
+  });
+}
+
+displayProductItems();
