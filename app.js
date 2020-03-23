dd34ef8a26bcde7ef73818925398b4bd2229f7be
Jun Huang <junyu_huang@hotmail.com>
2020-03-22 19:56:38 -0700

    complete register page and login page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const currentYear = document.querySelector(".footer__text--currentYear");
 currentYear.innerHTML = new Date().getFullYear();
 
-// nav menu, nav menu button, and nav menu button icon
+// nav menu, nav menu button, and nav menu button icon (only applies when user is in mobile or tablet view)
 const navMenu = document.querySelector(".nav-menu");
 const navMenuButton = document.querySelector(".nav-menu-button");
 let navMenuButtonIconHamburger = document.querySelector(
@@ -46,6 +46,7 @@ function toggleNavButton() {
   navMenuButtonIconClose.classList.toggle(
     "nav-menu-button-icon--close--active"
   );
+  // toggle nav menu
   navMenu.classList.toggle("nav-menu--active");
 }
 
@@ -101,39 +102,44 @@ const pensArray = [
   }
 ];
 
-const productSearchForm = document.querySelector(".search-form");
-const productSearchInput = document.querySelector(".input--search");
-const productCardGrid = document.querySelector(".card-grid-container");
+// only display and filter the products if the user is on the products page
+let productSearchForm = document.querySelector(".search-form");
+let productSearchInput = document.querySelector(".input--search");
+let productCardGrid = document.querySelector(".card-grid-container");
 
-const errorMessage = "not applicable";
+let isProductPage = productSearchForm && productSearchInput && productCardGrid;
+if (isProductPage) {
+  console.log(isProductPage);
 
-productSearchForm.addEventListener("submit", e => {
-  e.preventDefault(); // stop form from submitting or refreshing
-});
+  const errorMessage = "not applicable";
 
-productSearchInput.addEventListener("keyup", e => {
-  filterProducts(e.target.value);
-});
-
-function filterProducts(searchTerm) {
-  searchTerm = searchTerm.toLowerCase();
-  let allProducts = document.querySelectorAll(".card");
-  allProducts.forEach(product => {
-    const productName = product.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.firstChild.textContent.toLowerCase();
-
-    // hide products that don't match the search term
-    if (productName.indexOf(searchTerm) !== -1) {
-      product.style.display = "block";
-    } else {
-      product.style.display = "none";
-    }
+  productSearchForm.addEventListener("submit", e => {
+    e.preventDefault(); // stop form from submitting or refreshing
   });
-}
 
-function displayProductItems() {
-  pensArray.map(pen => {
-    // // onClick={() => setCurrentCountry(country)}
-    let card = `
+  productSearchInput.addEventListener("keyup", e => {
+    filterProducts(e.target.value);
+  });
+
+  function filterProducts(searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+    let allProducts = document.querySelectorAll(".card");
+    allProducts.forEach(product => {
+      const productName = product.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.firstChild.textContent.toLowerCase();
+
+      // hide products that don't match the search term
+      if (productName.indexOf(searchTerm) !== -1) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  }
+
+  function displayProductItems() {
+    pensArray.map(pen => {
+      // // onClick={() => setCurrentCountry(country)}
+      let card = `
       <a
         href="#"
         class="card"
@@ -151,8 +157,9 @@ function displayProductItems() {
         </section>
       </a>
   `;
-    productCardGrid.insertAdjacentHTML("beforeend", card);
-  });
-}
+      productCardGrid.insertAdjacentHTML("beforeend", card);
+    });
+  }
 
-displayProductItems();
+  displayProductItems();
+}
